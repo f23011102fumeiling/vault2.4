986fe885c6217694ca2c4ea61f87edb53bfce44e
fumeiling1 <15958631+fumeiling1@user.noreply.gitee.com>
2026-02-04 23:11:31 +0800

    Initial commit

--- backend/check_users.py
+++
@@ -1,9 +0,0 @@
-from app.database import SessionLocal
-from app.models.user import User
-
-db = SessionLocal()
-users = db.query(User).all()
-print('Current users in database:')
-for u in users:
-    print(f'  - ID: {u.id}, Username: {u.username}, Role: {u.role}')
-db.close()
